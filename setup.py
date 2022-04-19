4efe41e826756992fe850cc59d87df3144701b3b
Stefano Katsoras <stefano.katsoras@gmail.com>
2022-04-18 20:46:47 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="",
+    version="1.0.0",
+    author="Stefano Katsoras",
+    author_email="stefano.katsoras@gmail.com",
+    license="MIT",
+    packages=setuptools.find_packages(""),
+    install_requires=[""],
+)
